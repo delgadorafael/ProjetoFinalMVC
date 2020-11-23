7c8137b64b539d0de57f8f0d359449eb5ff71eb3
Rafael Delgado <rafa12del@hotmail.com>
2020-11-23 17:01:01 +0100

    CRUD Scaffolding Departments

---
+++ ProjetoFinalMVC/Data/ProjetoFinalMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoFinalMVC.Models;
+
+namespace ProjetoFinalMVC.Data
+{
+    public class ProjetoFinalMVCContext : DbContext
+    {
+        public ProjetoFinalMVCContext (DbContextOptions<ProjetoFinalMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoFinalMVC.Models.Department> Department { get; set; }
+    }
+}
