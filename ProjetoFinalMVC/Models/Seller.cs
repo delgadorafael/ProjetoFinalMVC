542691318ebd5200c08450208e6c7a7e9873582d
Rafael Delgado <rafa12del@hotmail.com>
2020-12-02 14:27:53 +0100

    Validations

--- ProjetoFinalMVC/Models/Seller.cs
+++ ProjetoFinalMVC/Models/Seller.cs
@@ -12,16 +12,28 @@ namespace ProjetoFinalMVC.Models
         //[Key]
         //[DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid e-mail")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.0, 50000.00, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
+        
         public Department Deparment { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
@@ -49,7 +61,7 @@ namespace ProjetoFinalMVC.Models
             Deparment = deparment;
         }
 
-        public void AddSales (SalesRecord salesRecord)
+        public void AddSales(SalesRecord salesRecord)
         {
             Sales.Add(salesRecord);
         }
